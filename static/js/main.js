2cc7b963123ed56ab61e01f1a3f8b8f841881e2e
Johnny Zhu <45405949+Johnnydaszhu@users.noreply.github.com>
2019-11-21 22:10:12 -0500

    center

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,16 @@
 $(".animated").addClass("delay-1s");
+
+$('.number').appear(function() {
+    $('.number').countTo();
+});
+
+
+import counterUp from 'counterup2'
+
+const el = document.querySelector( '.counter' )
+
+// Start counting, do this on DOM ready or with Waypoints.
+counterUp( el, {
+    duration: 1000,
+    delay: 16,
+} )
